22907c811114348214fee5c956ebceaefc3215f3
Dhiraj Wadile <114635434+Dhiraj-Wadile@users.noreply.github.com>
2023-06-04 21:11:47 +0530

    Update Ass3_Division.c

--- SE_IT 4th SEM/PSDL/Ass3_Division.c
+++ SE_IT 4th SEM/PSDL/Ass3_Division.c
@@ -1,27 +1,26 @@
 //Assignment 3-Dhiraj23361
 
-#include<pic18f458.h>
-#include<stdlib.h>
-#include<stdio.h>
-#include<xc.h>
+#include <pic18f458.h>
+#include <stdio.h>
+#include <xc.h>
 
+void main(void) {
+    int choice = 0x0F;
+    TMR1H = 0x0F;
+    TMR1L = 0x04;
+    TRISB = 0x00;
+    TRISC = 0x00;
 
-void main(void){
-   int choice=0x0F;
-   TMR1L=0xF;
-   TMR1H=0x2;
-   TRISB=0;
-   TRISC=0;
-while(1){
-     if(choice=1){
-        TMR0=TMR1L*TMR1H;
-        PORTB=TMR0L;
-        PORTC=TMR0H;
-}
-    if(choice=2){
-       PORTB=TMR1H/TMR1H;
-       PORTC=TMR1H%TMR1H;
-     }
-   }
-   return;
+    while (1) {
+        if (choice = 1) {
+            TMR0 = TMR1L * TMR1H;
+            PORTB = TMR0L;
+            PORTC = TMR0H;
+        }
+        if (choice = 2) {
+            PORTB= TMR1H/TMR1L;
+            PORTC =TMR1H%TMR1L;
+        }
+    }
+    return;
 }
